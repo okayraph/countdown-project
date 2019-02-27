7a5929bbe0efbabf7930087d23545d2a4da0d907
Okay Raph <okayraph@gmail.com>
2019-02-27 13:57:31 +0100

    countdown time

---
+++ script.js
@@ -0,0 +1,29 @@
+function redirect(){
+	window.location.href= "welcome.html"
+}
+function back(){
+	window.location.href= "index.html"
+}
+
+
+
+var countDownDate = new Date("Nov 6, 2019 00:00:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "My Birthday Countdown";
+  }
+}, 1000);
+
+
+var date = new Date();
+document.write(date);
